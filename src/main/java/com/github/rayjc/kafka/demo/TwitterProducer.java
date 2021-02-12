a147cbeab0da534981a0ce7f2ae66fab61b645c4
Ray Chung <ray.j.chung@gmail.com>
2021-02-11 22:52:42 -0800

    Add kafka producer to stream tweets.

--- src/main/java/com/github/rayjc/kafka/demo/TwitterProducer.java
+++ src/main/java/com/github/rayjc/kafka/demo/TwitterProducer.java
@@ -10,10 +10,13 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -23,14 +26,18 @@ public class TwitterProducer {
     private final String consumerSecretKey = System.getenv("TWITTER_SECRET_KEY");
     private final String token = System.getenv("TWITTER_TOKEN");
     private final String tokenSecret = System.getenv("TWITTER_TOKEN_SECRET");
+    private final String bootstrapServers = "127.0.0.1:9092";
 
     private Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
 
-    public TwitterProducer() {
+    private List<String> terms;
+
+    public TwitterProducer(List<String> terms) {
+        this.terms = terms;
     }
 
     public static void main(String[] args) {
-        new TwitterProducer().run();
+        new TwitterProducer(Lists.newArrayList("final fantasy", "kingdom hearts")).run();
     }
 
     public void run() {
@@ -39,9 +46,19 @@ public class TwitterProducer {
 
         // create twitter client
         Client client = createTwitterClient(msgQueue);
-        client.connect();
 
         // create kafka producer
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+        // shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping application...");
+            logger.info("Terminating Twitter client...");
+            client.stop();
+            logger.info("Terminating Kafka producer...");
+            producer.close();
+            logger.info("Finished...");
+        }));
 
         // stream tweets to kafka
         while (!client.isDone()) {
@@ -55,6 +72,15 @@ public class TwitterProducer {
 
             if (msg != null) {
                 logger.info(msg);
+                // remember to create a topic
+                producer.send(new ProducerRecord<>("twitter_tweets", null, msg), new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if (e != null) {
+                            logger.error("Got an error...", e);
+                        }
+                    }
+                });
             }
 
         }
@@ -62,12 +88,23 @@ public class TwitterProducer {
         logger.info("Application Exiting.");
     }
 
-    public Client createTwitterClient(BlockingQueue<String> msgQueue) {
+    private KafkaProducer<String, String> createKafkaProducer() {
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        return new KafkaProducer<String, String>(properties);
+    }
+
+    private Client createTwitterClient(BlockingQueue<String> msgQueue) {
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
         Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
         StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
         // Optional: set up some followings and track terms
-        List<String> terms = Lists.newArrayList("final fantasy");
+
         hosebirdEndpoint.trackTerms(terms);
 
         // These secrets should be read from a config file
@@ -81,6 +118,8 @@ public class TwitterProducer {
                 .processor(new StringDelimitedProcessor(msgQueue));
 
         Client hosebirdClient = builder.build();
+        // try to establish a connection
+        hosebirdClient.connect();
 
         return hosebirdClient;
     }
